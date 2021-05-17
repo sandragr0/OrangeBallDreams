4c5ed6058bc7f4d9cade2eef0f843103d4d2b18c
sandragr0 <sandraguerreror1995@gmail.com>
2021-05-17 11:08:31 +0200

    Changed admin.php

--- public/admin.php
+++ public/admin.php
@@ -2,18 +2,21 @@
 
 // Necesitaremos usar sesiones así que abrimos aquí la sesión
 session_start();
+include_once '../controller/admin/AdminControllerJugador.php';
+include_once '../controller/admin/AdminControllerUsuario.php';
 
-include_once '../controller/admin/AdminController.php';
-
-// Obtener parámetros de la sesión y la URL
+// Obtener parámetros de la sesión
 $usuario = isset($_SESSION['usuario']) ? $_SESSION['usuario'] : null;
+
+// Obtener controlador y acción solicitada
 $getController = isset($_GET['c']) ? ucfirst($_GET['c']) : null;
 $getAction = isset($_GET['a']) ? $_GET['a'] : null;
 
+$controllerName = "AdminController" . $getController;
+
 if ($usuario == null) {
-    $getController = "Usuario";
     $getAction = "login";
-    $controlador = new AdminController($getController);
+    $controlador = new AdminControllerUsuario();
     $controlador->$getAction();
 } else {
     if ($getController != null) {
@@ -21,15 +24,13 @@ if ($usuario == null) {
         if ($getAction == null) {
             $getAction = "list";
         }
-        $controlador = new AdminController($getController);
+        $controlador = new $controllerName();
         $controlador->$getAction();
     } else {
         if ($getAction == null) {
-
             // Si hay definido un usuario y no hay controller el default será Jugador
-            $getController = "Jugador";
             $getAction = "list";
-            $controlador = new AdminController($getController);
+            $controlador = new AdminControllerJugador();
             $controlador->$getAction();
         }
     }
