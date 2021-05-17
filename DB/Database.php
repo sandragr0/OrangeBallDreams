7156e0a08d5ab3c0eff40357e052a15eb92e7221
sandragr0 <sandraguerreror1995@gmail.com>
2021-05-17 11:06:48 +0200

    Changed database name

--- DB/Database.php
+++ DB/Database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function connect()
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=obd_proyecto;charset=utf8', 'root', '');
+        $pdo = new PDO('mysql:host=localhost;dbname=orangeballdreams;charset=utf8', 'root', '');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
     }
