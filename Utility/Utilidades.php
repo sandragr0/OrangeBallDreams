ebe41f8c6526ab164299a04969437d950c2d323e
sandragr0 <sandraguerreror1995@gmail.com>
2021-05-17 11:08:08 +0200

    Added new methods

--- Utility/Utilidades.php
+++ Utility/Utilidades.php
@@ -19,6 +19,27 @@ class Utilidades {
         return false;
     }
 
+    static function isString($string) {
+        if (preg_match("/(^[A-Za-zñÑá-úÁ-Ú]+$)/", $string)) {
+            return true;
+        }
+        return false;
+    }
+
+    static function isTelefono($string) {
+        if (preg_match("/(^[0-9]{9}+$)/", $string)) {
+            return true;
+        }
+        return false;
+    }
+
+    static function isAltura($string) {
+        if (preg_match("/(^[0-9]{1}[.]{1}[0-9]{2}+$)/", $string)) {
+            return true;
+        }
+        return false;
+    }
+
     static function isEmpty($string) {
         if ($string == "") {
             return true;
@@ -32,7 +53,50 @@ class Utilidades {
         }
         return false;
     }
+
+    static function isDNI($string) {
+        if (preg_match('/^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKEtrwagmyfpdxbnjzsqvhlcke]{1}$/', $string)) {
+            return true;
+        }
+        return false;
+    }
+
+    static function cleanString($string) {
+        return strip_tags(trim($string));
+    }
+
+    static function isFecha($fecha) {
+        $fechaArray = explode("-", $fecha);
+        if (count($fechaArray) == 3) {
+            if (is_numeric($fechaArray[0]) && is_numeric($fechaArray[1]) && is_numeric($fechaArray[2])) {
+                if (checkdate($fechaArray[1], $fechaArray[2], $fechaArray[0])) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    static function mb_ucfirst($string, $encoding = 'UTF-8') {
+        $stringLenght = mb_strlen($string, $encoding);
+        $primerCaracter = mb_substr($string, 0, 1, $encoding);
+        $resto = mb_substr($string, 1, $stringLenght - 1, $encoding);
+        return mb_strtoupper($primerCaracter, $encoding) . $resto;
+    }
     
-     
+   static function imgFormatoCorrecto($type) {
+       if ($type == "image/jpeg" || $type == "image/png") {
+           return true;
+       } 
+       return false;
+   }
+   
+   static function isValidImgSize($size) { 
+       if ($size <= 2097152) {
+           return true;
+       }
+       return false;
+   }
+   
 
 }
