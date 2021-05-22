6348ff7a24fbe32405eccdddfd9c6723ce0b4b7a
sandragr0 <sandraguerreror1995@gmail.com>
2021-05-22 11:08:18 +0200

    fixes

--- Utility/Utilidades.php
+++ Utility/Utilidades.php
@@ -1,28 +1,50 @@
 <?php
 
-class Utilidades {
+/**
+ * Class Utilidades
+ */
+abstract class Utilidades {
 
+    /**
+     * @return string
+     */
     static function getDocumentRoot() {
         return $_SERVER['DOCUMENT_ROOT'] . "/OrangeBallDreams";
     }
 
+    /**
+     * @param $fecha
+     * @return string
+     */
     static function fechaToFormulario($fecha) {
         $fechaArray = explode("-", $fecha);
         return $fechaArray[2] . "/" . $fechaArray[1] . "/" . $fechaArray[0];
     }
 
+    /**
+     * @param $fecha
+     * @return string
+     */
     static function fechaToBD($fecha) {
         $fechaArray = explode("/", $fecha);
         return $fechaArray[2] . "-" . $fechaArray[1] . "-" . $fechaArray[0];
     }
 
+    /**
+     * @param $string
+     * @return bool
+     */
     static function isAlpha($string) {
-        if (preg_match("/(^[A-Za-zñÑá-úÁ-Ú\s]+$)/", $string)) {
+        if (preg_match("/(^[A-Za-zñÑá-úÁ-Ú0-9\s]+$)/", $string)) {
             return true;
         }
         return false;
     }
 
+    /**
+     * @param $string
+     * @return bool
+     */
     static function isString($string) {
         if (preg_match("/(^[A-Za-zñÑá-úÁ-Ú]+$)/", $string)) {
             return true;
@@ -30,6 +52,10 @@ class Utilidades {
         return false;
     }
 
+    /**
+     * @param $string
+     * @return bool
+     */
     static function isTelefono($string) {
         if (preg_match("/(^[0-9]{9}+$)/", $string)) {
             return true;
@@ -37,6 +63,10 @@ class Utilidades {
         return false;
     }
 
+    /**
+     * @param $string
+     * @return bool
+     */
     static function isAltura($string) {
         if (preg_match("/(^[0-9]{1}[.]{1}[0-9]{2}+$)/", $string)) {
             return true;
@@ -44,6 +74,10 @@ class Utilidades {
         return false;
     }
 
+    /**
+     * @param $string
+     * @return bool
+     */
     static function isEmpty($string) {
         if ($string == "") {
             return true;
@@ -51,6 +85,10 @@ class Utilidades {
         return false;
     }
 
+    /**
+     * @param $string
+     * @return bool
+     */
     static function isValidPasswd($string) {
         if (preg_match("/((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))/", $string)) {
             return true;
@@ -58,6 +96,10 @@ class Utilidades {
         return false;
     }
 
+    /**
+     * @param $string
+     * @return bool
+     */
     static function isDNI($string) {
         if (preg_match('/^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKEtrwagmyfpdxbnjzsqvhlcke]{1}$/', $string)) {
             return true;
@@ -65,10 +107,18 @@ class Utilidades {
         return false;
     }
 
+    /**
+     * @param $string
+     * @return string
+     */
     static function cleanString($string) {
         return strip_tags(trim($string));
     }
 
+    /**
+     * @param $fecha
+     * @return bool
+     */
     static function isFecha($fecha) {
         $fechaArray = explode("-", $fecha);
         if (count($fechaArray) == 3) {
@@ -81,6 +131,11 @@ class Utilidades {
         return false;
     }
 
+    /**
+     * @param $string
+     * @param string $encoding
+     * @return string
+     */
     static function mb_ucfirst($string, $encoding = 'UTF-8') {
         $stringLenght = mb_strlen($string, $encoding);
         $primerCaracter = mb_substr($string, 0, 1, $encoding);
@@ -88,6 +143,10 @@ class Utilidades {
         return mb_strtoupper($primerCaracter, $encoding) . $resto;
     }
 
+    /**
+     * @param $type
+     * @return bool
+     */
     static function imgFormatoCorrecto($type) {
         if ($type == "image/jpeg" || $type == "image/png") {
             return true;
@@ -95,6 +154,10 @@ class Utilidades {
         return false;
     }
 
+    /**
+     * @param $size
+     * @return bool
+     */
     static function isValidImgSize($size) {
         if ($size <= 2097152) {
             return true;
