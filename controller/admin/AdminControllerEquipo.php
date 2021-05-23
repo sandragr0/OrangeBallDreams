91b2a3c6df46e5a2a5b4707aef0eafe662a30cb4
sandragr0 <sandraguerreror1995@gmail.com>
2021-05-23 12:39:48 +0200

    added edit equipo functionality to the equipo controller

--- controller/admin/AdminControllerEquipo.php
+++ controller/admin/AdminControllerEquipo.php
@@ -21,10 +21,8 @@ class AdminControllerEquipo extends AdminController
             include_once '../view/admin/admin-panel-footer.php';
         } else {
             $error = $this->validarEquipo($_POST);
-            echo $error;
             if ($error == 0) {
-                $equipo = new Equipo();
-                $equipo->setNombre(Utilidades::cleanString($_POST["nombre"]));
+                $equipo = $this->createEquipo($_POST);
                 try {
                     $this->model->add($equipo);
                     header('Location: admin.php?c=equipo&a=list');
@@ -47,6 +45,57 @@ class AdminControllerEquipo extends AdminController
         }
     }
 
+    public function edit()
+    {
+        if (isset($_REQUEST['id'])) {
+            $objeto = $this->model->view($_REQUEST['id']);
+        } else {
+            $objeto = null;
+        }
+
+        if ($objeto == null) {
+            include_once '../view/admin/admin-panel-header.php';
+            include_once "../view/admin/admin-view/error.php";
+            include_once '../view/admin/admin-panel-footer.php';
+        } else {
+            $jugadores = $this->model->getJugadores($_REQUEST['id']);
+            if (sizeof($_POST) == 0) {
+                include_once '../view/admin/admin-panel-header.php';
+                include_once "../view/admin/admin-view/editEquipo.php";
+                include_once '../view/admin/admin-panel-footer.php';
+            } else {
+                $error = $this->validarEquipo($_POST);
+                if ($error == 0) {
+                    $jugador = $this->createEquipo($_POST);
+                    try {
+                        $this->model->edit($_REQUEST["id"], $jugador);
+                        header('Location: admin.php?c=equipo&a=view&id=' . $_GET["id"]);
+                    } catch (Exception $e) {
+                        Utilidades::logError($e);
+                        if ($e->getCode() == 23000) {
+                            $db_error = CodigosError::db_duplicate_entry;
+                        } else {
+                            $db_error = CodigosError::db_generic_error;
+                        }
+                        include_once '../view/admin/admin-panel-header.php';
+                        include_once "../view/admin/admin-view/editEquipo.php";
+                        include_once '../view/admin/admin-panel-footer.php';
+                    }
+                } else {
+                    include_once '../view/admin/admin-panel-header.php';
+                    include_once "../view/admin/admin-view/editEquipo.php";
+                    include_once '../view/admin/admin-panel-footer.php';
+                }
+            }
+        }
+    }
+
+    private function createEquipo($datos) {
+        $equipo = new Equipo();
+        $equipo->setNombre(Utilidades::cleanString($datos["nombre"]));
+        return $equipo;
+    }
+
 
     private function validarEquipo($datos) {
         if (Utilidades::isEmpty($datos["nombre"])) {
@@ -65,10 +114,6 @@ class AdminControllerEquipo extends AdminController
         parent::list();
     }
 
-    public function edit()
-    {
-        parent::edit();
-    }
 
     public function view()
     {
@@ -95,6 +140,13 @@ class AdminControllerEquipo extends AdminController
     public function deleteJugador() {
         if (isset($_REQUEST['idJugador'])) {
             $this->model->deleteJugador($_REQUEST['idJugador']);
+        }
+        header('Location: admin.php?c=equipo&a=edit&id=' . $_GET["id"]);
+    }
+
+    public function deleteJugadorSetDispo() {
+        if (isset($_REQUEST['idJugador'])) {
+            $this->model->deleteJugadorSetDispo($_REQUEST['idJugador']);
         }
         header('Location: admin.php?c=equipo&a=view&id=' . $_GET["id"]);
     }
