bc44a1f64b0aeb3317e6ea1592e43671d963d537
sandragr0 <sandraguerreror1995@gmail.com>
2021-05-27 13:53:05 +0200

    refactores to make the error generic

--- view/admin/admin-view/addEstadisticaErrorJugador.php
+++
@@ -1,7 +0,0 @@
-<h1>Añadir estadistica</h1>
-<div class="card mb-3">
-    <div class="card-body">
-        Para añadir estadísticas debes añadir un jugador al menos, <a href="?c=jugador&a=add">¿quieres añadir un nuevo jugador?</a>
-    </div>
-</div>
-</main>
